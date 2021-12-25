9ab745865abdcd907ffe27945fdfb8d71b028879
Miles Lucas <mdlucas@hawaii.edu>
2021-12-24 21:28:55 -0600

    add combined method

--- src/LuckyImaging.jl
+++ src/LuckyImaging.jl
@@ -4,15 +4,20 @@ using Compat
 using Statistics
 using SubpixelRegistration
 
-export classic_lucky_image, classic_lucky_image!
+export lucky_image, classic_lucky_image, classic_lucky_image!
 
 include("util.jl")
 include("classic.jl")
 include("fourier.jl")
 
-function lucky_image(cube::AbstractArray; dims, q=0.9, alg=:fourier, metric=:com)
-
-
+function lucky_image(cube::AbstractArray{T,3}; alg=:fourier, kwargs...) where T
+    if alg === :classic
+        return classic_lucky_image(cube; kwargs...)
+    elseif alg === :fourier
+        return fourier_lucky_image(cube; kwargs...)
+    else
+        throw(ArgumentError("$alg not recognized as a lucky imaging algorithm. Choose from (:fourier, :classic)"))
+    end
 end
 
 end
